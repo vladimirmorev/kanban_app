dec8cac5cd5715a840948b8107aa7b954ab4fa58
vladimirmorev <v.morev90@gmail.com>
2016-03-07 21:04:46 +0600

    fix webpack.config.js

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,6 @@
-import './main.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-
 import alt from './libs/alt';
 import storage from './libs/storage';
 import persist from './libs/persist';
@@ -11,20 +8,3 @@ import persist from './libs/persist';
 persist(alt, storage, 'app');
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-module.exports = {
-  plugins: [
-    require('babel-plugin-syntax-class-properties'),
-    require('babel-plugin-syntax-decorators'),
-    require('babel-plugin-syntax-object-rest-spread'),
-
-    // You can pass parameters using an array syntax
-    [
-      require('babel-plugin-transform-regenerator'),
-      {
-        async: false,
-        asyncGenerators: false
-      }
-    ]
-  ]
-};
--- webpack.config.js
+++ webpack.config.js
@@ -6,53 +6,34 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
-
-// Load *package.json* so we can use `dependencies` from there
 const pkg = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
-
-process.env.BABEL_ENV = TARGET;
-
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
   style: path.join(__dirname, 'app/main.css')
 };
 
+process.env.BABEL_ENV = TARGET;
 
 const common = {
-
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
   entry: {
     app: PATHS.app,
     style: PATHS.style
   },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
-    // Output using entry name
     filename: '[name].js'
-  },  
+  },
   module: {
     loaders: [
       {
-        // Test expects a RegExp! Note the slashes!
-        test: /\.css$/,
-        loaders: ['style', 'css'],
-        // Include accepts either a path or an array of paths.
-        include: PATHS.app
-      },
-      {
         test: /\.jsx?$/,
-        // Enable caching for improved performance during development
-        // It uses default OS directory by default. If you need something
-        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
         loaders: ['babel?cacheDirectory'],
-        // Parse only app files! Without this it will go through entire project.
-        // In addition to being slow, that will most likely result in an error.
         include: PATHS.app
       }
     ]
@@ -67,42 +48,23 @@ const common = {
   ]
 };
 
-
-// Default configuration
 if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devtool: 'eval-source-map',
-
-    devServer: {     
-
-      // Enable history API fallback so HTML5 History API based
-      // routing works. This is a good default that will come
-      // in handy in more complicated setups.
+    devServer: {
       historyApiFallback: true,
       hot: true,
       inline: true,
       progress: true,
 
-      // Display only errors to reduce the amount of output.
+      // display only errors to reduce the amount of output
       stats: 'errors-only',
 
-      // Parse host and port from env so this is easy to customize.
-      //
-      // If you use Vagrant or Cloud9, set
-      // host: process.env.HOST || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices unlike default
-      // localhost
+      // parse host and port from env so this is easy
+      // to customize
       host: process.env.HOST,
       port: process.env.PORT
     },
-    plugins: [     
-      new webpack.HotModuleReplacementPlugin(),
-      new NpmInstallPlugin({
-        save: true // --save
-      })
-
-    ],
     module: {
       loaders: [
         // Define development specific CSS setup
@@ -113,12 +75,17 @@ if(TARGET === 'start' || !TARGET) {
         }
       ]
     },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new NpmInstallPlugin({
+        save: true // --save
+      })
+    ]
   });
 }
 
-if(TARGET === 'build') {
+if(TARGET === 'build' || TARGET === 'stats') {
   module.exports = merge(common, {
-    // Define vendor entry point needed for splitting
     entry: {
       vendor: Object.keys(pkg.dependencies).filter(function(v) {
         // Exclude alt-utils as it won't work with this setup
@@ -143,21 +110,16 @@ if(TARGET === 'build') {
       ]
     },
     plugins: [
-      // Extract vendor and manifest files
       new CleanPlugin([PATHS.build]),
-
+      // Output extracted CSS to a file
+      new ExtractTextPlugin('[name].[chunkhash].css'),
+      // Extract vendor and manifest files
       new webpack.optimize.CommonsChunkPlugin({
         names: ['vendor', 'manifest']
       }),
       // Setting DefinePlugin affects React library size!
-      // DefinePlugin replaces content "as is" so we need some extra quotes
-      // for the generated code to make sense
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': '"production"'
-
-        // You can set this to JSON.stringify('development') for your
-        // development target to force NODE_ENV to development mode
-        // no matter what
       }),
       new webpack.optimize.UglifyJsPlugin({
         compress: {
